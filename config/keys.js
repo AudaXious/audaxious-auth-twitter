49c77f29a090d970bb25582535f626a1f0b8334a
TinkerPal <tinkerpi2023@gmail.com>
2023-12-07 06:12:20 -0500

    fixes

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,8 @@
 const DBNAME = process.env.DBNAME;
 const DBPASSWORD = process.env.DBPASSWORD;
 const MONGODB = {
-  MONGODB_URI: `mongodb+srv://${DBNAME}:${DBPASSWORD}.zpijeny.mongodb.net/`,
+  MONGODB_URI: `mongodb+srv://${DBNAME}:${DBPASSWORD}@cluster0.o89pivv.mongodb.net/?retryWrites=true&w=majority`,
+  // MONGODB_URI: `mongodb+srv://${DBNAME}:${DBPASSWORD}.zpijeny.mongodb.net/`,
 };
 
 const SESSION = {
