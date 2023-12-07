a701058c15b409f74113d0f1b1c546ba5b073691
AudaXiousWeb3 <123374158+AudaXiousWeb3@users.noreply.github.com>
2023-12-07 08:58:38 -0500

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -1,7 +1,7 @@
 const DBNAME = process.env.DBNAME;
 const DBPASSWORD = process.env.DBPASSWORD;
 const MONGODB = {
-  MONGODB_URI: `mongodb+srv://${DBNAME}:${DBPASSWORD}@cluster0.o89pivv.mongodb.net/?retryWrites=true&w=majority`,
+  MONGODB_URI: `mongodb+srv://audaxious:GwRTH3cGpCtE7X9Z@cluster0.jj7jufa.mongodb.net/?retryWrites=true&w=majority`,
   // MONGODB_URI: `mongodb+srv://${DBNAME}:${DBPASSWORD}.zpijeny.mongodb.net/`,
 };
 
